f5d820e8790e76846bdb45960cdcdb476880c193
ELANSARI05 <Mouad.ELANSARI@um6p.ma>
2026-02-05 00:23:09 +0100

    TP2

---
+++ TP2/ex2/optimized_ex3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 100000000
+
+int main() {
+ double a = 1.1, b = 1.2;
+ double x = 0.0, y = 0.0,temp = 0.0;
+ clock_t start, end;
+
+ start = clock();
+ for (int i = 0; i < N; i++) {
+    temp= a * b;
+    x += temp;
+    y += temp;
+}
+
+ end = clock();
+
+ printf("x␣=␣%f,␣y␣=␣%f,␣time␣=␣%f␣s\n",
+ x, y, (double)(end - start) / CLOCKS_PER_SEC);
+ return 0;
+ }
